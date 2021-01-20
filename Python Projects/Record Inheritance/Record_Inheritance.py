169242a9c9e208300f8ca91e2260bfd30cd635a1
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2021-01-20 13:30:54 -0800

    Add files via upload

---
+++ Python Projects/Record Inheritance/Record_Inheritance.py
@@ -0,0 +1,11 @@
+class Music:
+
+    def __init__(self, album, artist, year, genre):
+        self.__album = album
+        self.__artist = artist
+        self.__year = year
+        self.__genre = genre
+
+        #Mutators
+
+        def set))
