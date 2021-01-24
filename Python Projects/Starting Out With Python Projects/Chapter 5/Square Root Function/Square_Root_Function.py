1d965f85790a5d7cfd72c154fa8012859d1593f1
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2021-01-23 19:29:00 -0800

    Add files via upload

---
+++ Python Projects/Starting Out With Python Projects/Chapter 5/Square Root Function/Square_Root_Function.py
@@ -0,0 +1,17 @@
+#Square Root Function
+import math
+
+
+def main():
+    #Get Number
+    number = float(input('Type a Number'))
+
+    #Get Square Root
+    sqRoot = math.sqrt(number)
+
+    #Display Answer
+    print('Square Root is', number, '0 is', sqRoot)
+
+    #Calling Main Function
+    main()
+
